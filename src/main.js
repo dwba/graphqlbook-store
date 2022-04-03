ad17531aa0f85721bbcd3376863c017f5d73617a
homeProjects <passmore@computer.org>
2022-04-03 16:36:36 +0100

    Init before converting Books to northwind

--- src/main.js
+++ src/main.js
@@ -1,8 +1,8 @@
 import Vue from 'vue';
 import App from './layout/App.vue';
 import router from './router';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'bootstrap';
+// import 'bootstrap/dist/css/bootstrap.min.css';
 import './registerServiceWorker';
 import VueApollo from "vue-apollo";
 import ApolloClient from 'apollo-boost';
@@ -10,10 +10,13 @@ import gql from 'graphql-tag';
 
 
 // create a provider
+// launch postgraphile with
+// npx postgraphile -c postgres://postgres:jmpg@localhost:5432/northwind --watch --enhance-graphiql --dynamic-json
 
 const apolloProvider = new VueApollo({
   defaultClient : new ApolloClient({
-    uri: 'https://tender-meerkat-56.hasura.app/v1/graphql'
+//    uri: 'https://tender-meerkat-56.hasura.app/v1/graphql'
+    uri: 'http://localhost:5000/graphql'
   })
 })
 
